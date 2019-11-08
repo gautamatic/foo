d2a9ce0f9647bd07ba4a7705d9e61a58d03e770c
gautamatic <37250145+gautamatic@users.noreply.github.com>
2019-11-08 10:41:49 -0500

    Update hello2.cs

--- hello2.cs
+++ hello2.cs
@@ -3,16 +3,18 @@ public class Calculator
 
     // adds the second number to the first
     // and returns the resulting value    
+    // and adds one
     public int Add(int a, int b)
     {
-        return a + b;
+        return a + b + 1;
     }
 
     // subtracts the second number from the first
     // and returns the resulting value
+    // adds one
     public int Subtract(int a, int b)
     {
-        return a - b;
+        return a - b + 1;
     }
 
     public int MyCustomCalculation(int a, int b)
@@ -23,15 +25,14 @@ public class Calculator
         {
             Console.WriteLine("this will require a manager approval");
         }
-        else
+        else if (x + y < 50)
         {
             Console.WriteLine("this is okay for you to approve");
         }
+        else 
+        {
+            // do nothing 
+        }
         return (x * y) + (a + b);
-    }
-
-    public int Multiply(int a, int b)
-    {
-        return a * b + 1;
     }
 }
